d202a8cd71b7f62d9d2fbcceb61375851c101f71
fedebayle <fedebayle@gmail.com>
2021-04-28 12:45:06 -0300

    ndvi and concat images scripts

---
+++ concat_images.py
@@ -0,0 +1,12 @@
+import subprocess
+import glob
+import os
+tile = '0000000000-0000010496'
+images_ndvi = sorted(glob.glob(
+    '/home/gis_dm_2021/tp-2/images/s2full/aoi/**/{tile}_ndvi.tif'.format(tile=tile)))
+images_ndvi_otb = ' '.join(images_ndvi)
+im_out_fname = '/home/gis_dm_2021/tp-2/images/results/{tile}_ndvi.tif'.format(
+    tile=tile)
+cmd = """bash -c 'source ~/OTB-7.2.0-Linux64/otbenv.profile;  otbcli_ConcatenateImages -il {im} -out {im_out}'""".format(
+    im=images_ndvi_otb, im_out=im_out_fname)
+print(cmd)
---
+++ run_ndvi.py
@@ -0,0 +1,11 @@
+import subprocess
+import glob
+import os
+images = glob.glob('/home/gis_dm_2021/tp-2/images/s2full/aoi/**/*.tif')
+for image in images:
+    fname, ext = os.path.splitext(image)
+    im_out_fname = '{im}_ndvi.tif'.format(im=fname)
+    cmd = """bash -c 'source ~/OTB-7.2.0-Linux64/otbenv.profile;  otbcli_BandMath -il {im} -out {im_out} -exp "(im1b7-im1b3)/(im1b7+im1b3)"'""".format(
+        im=image, im_out=im_out_fname)
+    print(cmd)
+    #subprocess.run(cmd, shell=True)
