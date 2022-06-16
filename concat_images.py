435aba266c180532a3208e2ec31af02b2ac57daf
fedebayle <fedebayle@gmail.com>
2022-06-16 19:41:32 -0300

    Update concat_images.py

--- concat_images.py
+++ concat_images.py
@@ -3,10 +3,10 @@ import glob
 import os
 tile = '0000000000-0000012544'
 images_ndvi = sorted(glob.glob(
-    '/home/gis_dm_2021/tp-2/images/s2full/aoi/**/{tile}_ndvi.tif'.format(tile=tile)))
+    '/home/gis_dm_2022/tp-2/images/s2full/aoi/**/{tile}_ndvi.tif'.format(tile=tile)))
 images_ndvi_otb = ' '.join(images_ndvi)
-im_out_fname = '/home/gis_dm_2021/tp-2/images/results/{tile}_ndvi.tif'.format(
+im_out_fname = '/home/gis_dm_2022/tp-2/images/results/{tile}_ndvi.tif'.format(
     tile=tile)
-cmd = """bash -c 'source ~/OTB-7.2.0-Linux64/otbenv.profile;  otbcli_ConcatenateImages -il {im} -out {im_out}'""".format(
+cmd = """bash -c 'source ~/OTB-8.0.1-Linux64/otbenv.profile;  otbcli_ConcatenateImages -il {im} -out {im_out}'""".format(
     im=images_ndvi_otb, im_out=im_out_fname)
 print(cmd)
